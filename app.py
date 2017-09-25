8d2aa9809ec4791ca6510e8ccfbb80c3335acb0d
Joyce Wu <jwu26@stuy.edu>
2017-09-25 19:16:02 -0400

    added notes and header

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
+# Joyce Wu
+# Softdev1 pd 7
+# HW#04: Fill up yer Flask
+# 2017-09-24
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/") #route 0
+def index(): #creates an index linking the three different routes using html
     return '<h1>Choose your destiny:<br><a href="/happiness">A</a> <a href="/love">B</a> <a href="/passion">C</a></h1>'
 
-@app.route("/happiness")
+@app.route("/happiness") #route 1
 def happy():
     return "Can you really find happiness in life?"
 
-@app.route("/love")
+@app.route("/love") #route 2
 def love():
     return "What is love?"
 
-@app.route("/passion")
+@app.route("/passion") #route 3
 def passion():
     return "What do you do on your free time?"
 
